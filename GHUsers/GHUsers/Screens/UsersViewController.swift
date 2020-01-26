96b42268a89fe9d029ddaec1f55297f0532d209b
Le0nX <cloudmindone@gmail.com>
2020-01-26 16:59:16 +0300

    feature: flow layout

--- GHUsers/GHUsers/Screens/UsersViewController.swift
+++ GHUsers/GHUsers/Screens/UsersViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class UsersViewController: UIViewController {
     var userName: String!
-    
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        
+        configureCollectionView()
+        configureVC()
+        getFollowers()
+    }
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: userName, page: 1, completed: { result in
             
             switch result {
@@ -28,6 +30,34 @@ class UsersViewController: UIViewController {
                 print(followers)
             }
         })
+    }
+    
+    func configureVC() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        // TODO: refactor
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumSpacing: CGFloat = 10
+        
+        let aviliableWidth = width - (padding * 2) - (minimumSpacing * 2)
+        let itemWidth = aviliableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
     }
     
 
